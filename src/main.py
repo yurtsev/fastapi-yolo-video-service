d6f86659d3eb9a5370f3d74d877fa4ca504cb4f3
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-04-17 18:24:56 +0300

    chore: mount video router in main application

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from src.load_video.from_url.router import router as video_router
 
 
 app = FastAPI()
-
+app.include_router(video_router, prefix="/video", tags=["Video"])
 
 @app.get("/")
 async def ping():
