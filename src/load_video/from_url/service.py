6ea49a0f31e52a9b74c669c3608c46513465d45c
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-04-23 03:18:20 +0300

    refactor(video): support downloading videos from more services than YouTube

--- src/load_video/from_url/service.py
+++ src/load_video/from_url/service.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import tempfile
+import yt_dlp
 
 def youtube_video(url: str) -> str:
     yt = YouTube(url)
@@ -9,3 +10,12 @@ def youtube_video(url: str) -> str:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
         stream.download(filename=temp_file.name)
         return temp_file.name
+
+
+def download_video(url: str) -> str:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+        ydl_opts = {"outtmpl": temp_file.name, "format": "best[ext=mp4]/best", "quiet": True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        return temp_file.name
+
