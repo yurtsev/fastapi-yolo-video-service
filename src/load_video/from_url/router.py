c25179a19d354e3506f15b343e35adc1b463ca8b
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-20 02:16:37 +0300

    refactor(router): minor code cleanup and formatting

--- src/load_video/from_url/router.py
+++ src/load_video/from_url/router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
-from src.track_video.schemas import VideoRequest
-from src.track_video.service import annotate_video
-from src.load_video.from_url.service import download_video_url
 
+from src.load_video.from_url import download_video_url
+from src.track_video import VideoRequest, VideoResponse
+from src.track_video import annotate_video
 
 router = APIRouter()
 
 
-@router.post("/url")
-def download_video_from_url(data: VideoRequest) -> str:
+@router.post("/url", response_model=VideoResponse)
+def download_video_from_url(data: VideoRequest) -> VideoResponse:
     video_path = download_video_url(str(data.url))
-    return annotate_video(video_path, data.objects)
+    return annotate_video(str(video_path), data.objects)
