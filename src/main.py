a378cd90bcd4a5545f5623cdb999f33fed32b4a4
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-24 02:50:06 +0300

    chore(main): add standalone launch with uvicorn

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from src.load_video import router as upload_router
@@ -9,3 +10,7 @@ app.include_router(upload_router, prefix="/video", tags=["Video"])
 @app.get("/")
 async def ping():
     return "pong"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
