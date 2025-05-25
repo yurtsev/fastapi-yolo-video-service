a171fd2fd831cbbefc01b72a8ccbce1fb0de9cf8
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-25 04:25:46 +0300

    refactor(router): switch to new router_annotate_video function

--- src/load_video/router.py
+++ src/load_video/router.py
@@ -2,8 +2,12 @@ from typing import List, Optional
 
 from fastapi import APIRouter, UploadFile
 
-from src.load_video.service import download_video_pc, download_video_url
-from src.track_video import VideoRequest, VideoResponse, annotate_video
+from src.load_video.schemas import VideoRequest, VideoResponse
+from src.load_video.service import (
+    download_video_pc,
+    download_video_url,
+    router_annotate_video,
+)
 
 router = APIRouter()
 
@@ -11,11 +15,12 @@ router = APIRouter()
 @router.post("/url", response_model=VideoResponse)
 async def download_video_from_url(data: VideoRequest) -> VideoResponse:
     video_path = download_video_url(str(data.url))
-    return annotate_video(str(video_path), data.objects)
+    return router_annotate_video(video_path, data.objects)
 
 
 @router.post("/pc", response_model=VideoResponse)
 async def download_video_from_pc(
     uploaded_file: UploadFile, data: Optional[List[str]] = None
 ) -> VideoResponse:
-    return annotate_video(str(download_video_pc(uploaded_file)), data)
+    file_path = download_video_pc(uploaded_file)
+    return router_annotate_video(file_path, data)
