3f1846454d268b8a42783fcafaadb45320f694a6
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-25 04:28:02 +0300

    update init file

--- src/load_video/__init__.py
+++ src/load_video/__init__.py
@@ -1,8 +1,5 @@
 from src.load_video.router import router
-from src.load_video.service import download_video_pc, download_video_url
 
 _all_ = [
     "router",
-    "download_video_pc",
-    "download_video_url",
 ]
