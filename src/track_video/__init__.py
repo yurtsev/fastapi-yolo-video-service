421be52b3d3ec09246e89d6d08a73f80269e8ca8
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-25 04:02:56 +0300

    update init file

--- src/track_video/__init__.py
+++ src/track_video/__init__.py
@@ -1,8 +1,5 @@
-from src.track_video.schemas import VideoResponse, VideoRequest
 from src.track_video.service import annotate_video
 
 __all__ = [
-    "VideoRequest",
-    "VideoResponse",
     "annotate_video",
 ]
