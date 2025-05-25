42acd8b5e2b40a9929c330109a1f4dca48abcc1a
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-05-25 04:30:11 +0300

    feat(config): add Redis configuration variables

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     MINIO_ENDPOINT: str
@@ -6,10 +7,14 @@ class Settings(BaseSettings):
     MINIO_SECRET_KEY: str
     MINIO_BUCKET_NAME: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int = 6379
+
     model_config = {
         "env_file": ".env",
         "env_file_encoding": "utf-8",
         "extra": "ignore",
     }
+
 
 settings = Settings()
